18c08f98834ad59d3319a5e22f73249ecd6c464d
phdljr <phdljr@naver.com>
2023-12-22 15:42:47 +0900

    Test: test

---
+++ src/test/java/kr/ac/phdljr/boardrefactor/BoardRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.phdljr.boardrefactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardRefactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
