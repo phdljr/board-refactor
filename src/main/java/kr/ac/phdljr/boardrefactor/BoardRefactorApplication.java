89612b2ab36c1deb6bcb50b08cdb5f43c6e371e6
phdljr <phdljr@naver.com>
2023-12-22 14:59:46 +0900

    init

---
+++ src/main/java/kr/ac/phdljr/boardrefactor/BoardRefactorApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.phdljr.boardrefactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardRefactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardRefactorApplication.class, args);
+    }
+
+}
