ef5da128d8f1419f0b1058d5d6656b442d7089b9
phdljr <phdljr@naver.com>
2023-12-22 15:00:27 +0900

    Feat: add config

---
+++ src/main/java/kr/ac/phdljr/boardrefactor/global/config/RedisConfig.java
@@ -0,0 +1,10 @@
+package kr.ac.phdljr.boardrefactor.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+    
+}
